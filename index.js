1425e9d7e12b695779a92493ef645ff144a89ee5
Rushikesh Pandit <rushikeshpandit22@gmail.com>
2018-11-21 19:20:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const ET_Client = require('fuelsdk-node');
 const client = new ET_Client('tzv6fcoi1oachkygw2hnvva9', 'l0sUcKPSJMZEHYxJE68eQG0v','s7');
+var http = require('http');
+
 //console.log(client);
 /*const props = {
     Name: 'PandaTestDE5',
@@ -24,6 +26,14 @@ const props = ['Name']; 
 client.dataExtensionRow({props, Name: 'PandaAPIDE3'}).get((err,response) => { 
   console.log('\n Main Repsonce = '+JSON.stringify(response) + '\n Error = ' + JSON.stringify(err));
   console.log("\nresponse *****************************************************\n"+JSON.stringify(response.body.Results[0].Properties.Property)); 
-   var result = document.createElement("p");
-    result.innerText = JSON.stringify(response.body.Results[0].Properties.Property);
+    var userCount = 0;
+    http.createServer(function (request, response) {
+        console.log('New connection');
+        userCount++;
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(JSON.stringify(response.body.Results[0].Properties.Property));
+        response.end();
+    }).listen(8080);
+    console.log('Server started');
+    
 })
