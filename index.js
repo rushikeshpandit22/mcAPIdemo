38beccc81837e4b93c343e9ddf319f974cee90da
Rushikesh Pandit <rushikeshpandit22@gmail.com>
2018-12-10 14:56:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,54 @@ pg.defaults.ssl = true;
 pg.defaults.poolSize = 20;
 var client = new pg.Client(conString);
 client.connect();
+var accessToken = "";
+var XML1 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+              <soapenv:Header>
+              <fueloauth>`+accessToken+`</fueloauth>
+              </soapenv:Header>
+              <soapenv:Body>
+              <CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">
+              <Options/>
+              <Objects xsi:type="DataExtension">
+              <Client>
+              <ID>7291811</ID>
+              </Client>
+              <Name>DE_Panda8888</Name>
+              <CustomerKey>DE_Panda8888</CustomerKey>
+              <Description>A place to track of what triggers are purposefully in an inactive state</Description>
+              <IsSendable>false</IsSendable>
+              <Fields>
+              <Field>
+              <Name>TSDCustomerKey</Name>
+              <MaxLength>50</MaxLength>
+              <IsRequired>true</IsRequired>
+              <Ordinal>1</Ordinal>
+              <IsPrimaryKey>true</IsPrimaryKey>
+              <FieldType>Text</FieldType>
+              </Field>
+              <Field>
+              <Name>Status</Name>
+              <MaxLength>20</MaxLength>
+              <IsRequired>true</IsRequired>
+              <Ordinal>2</Ordinal>
+              <IsPrimaryKey>false</IsPrimaryKey>
+              <FieldType>Text</FieldType>
+              </Field>
+              <Field>
+              <Name>ModifiedDate</Name>
+              <Scale>0</Scale>
+              <DefaultValue>GetDate()</DefaultValue>
+              <IsRequired>true</IsRequired>
+              <Ordinal>3</Ordinal>
+              <IsPrimaryKey>false</IsPrimaryKey>
+              <FieldType>Date</FieldType>
+              </Field>
+              </Fields>
+              </Objects>
+              </CreateRequest>
+              </soapenv:Body>
+              </soapenv:Envelope>`
+
 
 
 app.post('/', (req, res) => {
@@ -43,30 +91,48 @@ app.post('/', (req, res) => {
        var authEndpoint = decode.request.rest.authEndpoint;
        var apiEndpointBase = decode.request.rest.apiEndpointBase; 
    // const client = new ET_Client('e91wco2s002d3dfz70r3m9f0', 'emdG17BLX14drPPNQ6QGmxMt', 's7');   
+
+       
       axios.post(authEndpoint,{
             "clientSecret":"emdG17BLX14drPPNQ6QGmxMt",
             "clientId":"e91wco2s002d3dfz70r3m9f0",
             "refreshToken":refreshToken,
             "accessType": "offline"        
         }).then(response =>{
-            var accessToken = response.data.accessToken;
+            accessToken = response.data.accessToken;
             refreshToken = response.data.refreshToken;
             console.log(accessToken);
+             //------------SOAP API------------------------------------------------------------------------------------------------------------
+       if(accessToken){
+              axios({
+                     method: 'post',
+                     url: 'https://webservice.s7.exacttarget.com/Service.asmx',
+                     headers: {
+                     "Content-Type":"text/xml;charset=UTF-8" ,
+                     "Accept-Encoding":"gzip,deflate",
+                     "SOAPAction":"Create"
+                     },
+                     data: XML1,
+              }).then((response) => {
+                     console.log(response.data);
+              }).catch((error) => {
+                     console.log(error);
+              });
+       }else{
+              console.log("Access Token not found");
+       }     
+             
+            //------------------------------------------------------------------------------------------------------------------------ 
             //apiEndpointBase+'/hub/v1/campaigns'
             //'https://mcs53v5db9s0nn0nrb3kgsl9qly1.rest.marketingcloudapis.com/hub/v1/campaigns'
              //axios.get(apiEndpointBase+'/hub/v1/campaigns',{
-             axios.get(apiEndpointBase+'data/v1/customobjectdata/key/AMPScript/rowset/',{
+          /*   axios.get(apiEndpointBase+'data/v1/customobjectdata/key/AMPScript/rowset/',{
                 headers: {
                 "content-type": 'application/json',
                 "authorization": "Bearer "+ accessToken
                 }}).then(response => {
                     console.log("================================");
                     console.log(JSON.stringify(response.data.items));
-                    /* const query = {
-                            text: 'INSERT INTO sfmctest.demo("Name", "Email") VALUES($1, $2)',
-                            values:response.data.items, //['panda4', 'panda@gmail4.com],
-                            
-                     }*/
                     const query = { 
                             text: 'insert into sfmctest.demo ("Name", "Email") select keys, values from jsonb_to_recordset(\'' + JSON.stringify(response.data.items) + '\') r(keys character varying , values character varying)'
                     }
@@ -82,7 +148,7 @@ app.post('/', (req, res) => {
                 }).catch( error => {
                     console.log("Get Campaigns ERROR");
                     console.log(error);
-                });
+                });*/
             
         }).catch( error => {
         console.log("Get AccessToken ERROR");
