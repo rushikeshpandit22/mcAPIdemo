24d9a6e8d7c2cb18746ae4ec95e6d72ab184cdc0
Rushikesh Pandit <rushikeshpandit22@gmail.com>
2018-12-10 17:22:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,32 +11,60 @@ app.set('view engine','pug');
 var port = process.env.PORT || 3000;
 var jwt = require('jwt-simple');
 var axios = require('axios');
-
 var pg = require('pg');
+
+
 var conString = "postgres://fpqxmoywayesyg:ae337dd3d3a2dd3f8c529e90091b04821f27530d21b2968bf580fee74dfcdc25@ec2-54-83-8-246.compute-1.amazonaws.com:5432/d8rb6oab555lft";
 pg.defaults.ssl = true;
 pg.defaults.poolSize = 20;
 var client = new pg.Client(conString);
 client.connect();
-var accessToken = "";
+var accessToken = "",dataExtentionXML, queryXML, automationXML, SoapPreHeader, FinalXML = "";
 
+SoapPreHeader = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+              <soapenv:Header>
+              <fueloauth>`;
 
+dataExtentionXML = `</fueloauth></soapenv:Header><soapenv:Body><CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI"><Options/><Objects xsi:type="DataExtension">
+                     <Client>
+                     <ID>7291811</ID>
+                     </Client>
+                     <Name>SFMC_App_Demo_DE</Name>
+                     <CustomerKey>SFMC_App_Demo_DE_API</CustomerKey>
+                     <Description>A place to track of what triggers are purposefully in an inactive state</Description>
+                     <IsSendable>false</IsSendable>
+                     <Fields>
+                     <Field>
+                     <Name>CustomerEmail</Name>
+                     <MaxLength>50</MaxLength>
+                     <IsRequired>true</IsRequired>
+                     <Ordinal>1</Ordinal>
+                     <IsPrimaryKey>true</IsPrimaryKey>
+                     <FieldType>Email</FieldType>
+                     </Field></Fields></Objects></CreateRequest></soapenv:Body></soapenv:Envelope>`;
+
+queryXML = `</fueloauth><a:Action s:mustUnderstand="1">Create</a:Action><a:To s:mustUnderstand="1">https://webservice.s7.exacttarget.com/Service.asmx</a:To></soapenv:Header>
+   <soapenv:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+      <CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">
+         <Options></Options>
+         <Objects xsi:type="QueryDefinition">
+            <PartnerKey xsi:nil="true"></PartnerKey>
+            <ObjectID xsi:nil="true"></ObjectID>
+            <CustomerKey>API QUERY ACTIVITY</CustomerKey>
+            <Name>API QUERY ACTIVITY</Name>
+            <Description>API QUERY ACTIVITY</Description>
+            <QueryText>Select EmailAddress as EMAIL from [_Subscribers]</QueryText>
+            <TargetType>DE</TargetType>
+            <DataExtensionTarget>
+               <PartnerKey xsi:nil="true"></PartnerKey>
+               <ObjectID xsi:nil="true"></ObjectID>
+               <CustomerKey>DemoAPi</CustomerKey>
+               <Name>Demo</Name>
+            </DataExtensionTarget>
+            <TargetUpdateType>Overwrite</TargetUpdateType>
+         </Objects></CreateRequest></soapenv:Body></soapenv:Envelope>`;
 
 app.post('/', (req, res) => {
-       //var a = [{ Name:'panda3', Email:'panda@gmail3.com'},{Name:'panda4', Email:'panda@gmail4.com'},{Name:'panda6', Email:'panda@gmail6.com'}];
-      /* const query = {
-              text: 'INSERT INTO sfmctest.demo("Name", "Email") VALUES($1, $2)',
-              values:['panda4', 'panda@gmail4.com],
-       }
-       client.query(query,(err, res) => {
-              if (err) {
-                     console.log(err.stack)
-              } else {
-                     console.log("Data inserted");  
-                     console.log(res.rowCount);
-              }
-       })*/
-   
        var jwtToken = req.body.jwt;
        var secret = 
        'aPwkRdSokdV9CsilYuZN3StLLeaKPUbFZRmptaoDcpFiEa2pUMSPNfbniKG3p06IlFak9TKz9CW0tTnlt1xuSybQkV3kCjGWN8cCsxyAGfHcb_050k-XplzYQyAJLwuYHBzBuU8w0FUbMRij64HjYljIwniEwlry348T3PDBIbPpq5qLGbWgdnOaTiG5SBW4qigC5ALKgSIArrPYvZgPBZS1TKGpm5cs4K-OQ3v7j_q1-qDawDQzSKN9Fdtj5g2';
@@ -44,10 +72,8 @@ app.post('/', (req, res) => {
        var refreshToken = decode.request.rest.refreshToken;
        var authEndpoint = decode.request.rest.authEndpoint;
        var apiEndpointBase = decode.request.rest.apiEndpointBase; 
-   // const client = new ET_Client('e91wco2s002d3dfz70r3m9f0', 'emdG17BLX14drPPNQ6QGmxMt', 's7');   
-
-       
-      axios.post(authEndpoint,{
+  
+       axios.post(authEndpoint,{
             "clientSecret":"emdG17BLX14drPPNQ6QGmxMt",
             "clientId":"e91wco2s002d3dfz70r3m9f0",
             "refreshToken":refreshToken,
@@ -56,55 +82,10 @@ app.post('/', (req, res) => {
             accessToken = response.data.accessToken;
             refreshToken = response.data.refreshToken;
             console.log(accessToken);
-             //------------SOAP API------------------------------------------------------------------------------------------------------------
-      /* var XML1 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-              <soapenv:Header>
-              <fueloauth>`+accessToken+`</fueloauth>
-              </soapenv:Header>
-              <soapenv:Body>
-              <CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">
-              <Options/>
-              <Objects xsi:type="DataExtension">
-              <Client>
-              <ID>7291811</ID>
-              </Client>
-              <Name>DE_Panda8888</Name>
-              <CustomerKey>DE_Panda8888</CustomerKey>
-              <Description>A place to track of what triggers are purposefully in an inactive state</Description>
-              <IsSendable>false</IsSendable>
-              <Fields>
-              <Field>
-              <Name>TSDCustomerKey</Name>
-              <MaxLength>50</MaxLength>
-              <IsRequired>true</IsRequired>
-              <Ordinal>1</Ordinal>
-              <IsPrimaryKey>true</IsPrimaryKey>
-              <FieldType>Text</FieldType>
-              </Field>
-              <Field>
-              <Name>Status</Name>
-              <MaxLength>20</MaxLength>
-              <IsRequired>true</IsRequired>
-              <Ordinal>2</Ordinal>
-              <IsPrimaryKey>false</IsPrimaryKey>
-              <FieldType>Text</FieldType>
-              </Field>
-              <Field>
-              <Name>ModifiedDate</Name>
-              <Scale>0</Scale>
-              <DefaultValue>GetDate()</DefaultValue>
-              <IsRequired>true</IsRequired>
-              <Ordinal>3</Ordinal>
-              <IsPrimaryKey>false</IsPrimaryKey>
-              <FieldType>Date</FieldType>
-              </Field>
-              </Fields>
-              </Objects>
-              </CreateRequest>
-              </soapenv:Body>
-              </soapenv:Envelope>`
-
+//------------SOAP API------------------------------------------------------------------------------------------------------------
+     
        if(accessToken){
+             FinalXML = SoapPreHeader+accessToken+dataExtentionXML; 
               axios({
                      method: 'post',
                      url: 'https://webservice.s7.exacttarget.com/Service.asmx',
@@ -113,17 +94,19 @@ app.post('/', (req, res) => {
                      "Accept-Encoding":"gzip,deflate",
                      "SOAPAction":"Create"
                      },
-                     data: XML1,
+                     data: FinalXML,
               }).then((response) => {
+                     consol.log("DataExtention Created\n");
                      console.log(response.data);
               }).catch((error) => {
+                     Console.log("DataExtention Creation Error=\n");
                      console.log(error);
               });
        }else{
               console.log("Access Token not found");
        }     */
              
-            //------------------------------------------------------------------------------------------------------------------------ 
+//-----------------------Rest_API------------------------------------------------------------------------------------------------- 
             //apiEndpointBase+'/hub/v1/campaigns'
             //'https://mcs53v5db9s0nn0nrb3kgsl9qly1.rest.marketingcloudapis.com/hub/v1/campaigns'
              //axios.get(apiEndpointBase+'/hub/v1/campaigns',{
@@ -156,28 +139,7 @@ app.post('/', (req, res) => {
         console.log(error);
         });
     
-    
-    
-    
-   /* const props = { 
-        Name:'happyPanda', 
-        Description:'happyPanda', 
-        CustomerKey:'happyPandaXoXo', 
-        Fields : { 
-            Field : { 
-                Name: 'Name', 
-                FieldType: 'Text', 
-                IsPrimaryKey: true, 
-                MaxLength: 100, 
-                IsRequired: true 
-            } 
-            } 
-    }; 
-    client.dataExtension({props}).post((err,response) => { 
-        console.log(JSON.stringify(response)); 
-        console.log("\n"+JSON.stringify(err)); 
-    });*/
-
+ 
         res.sendFile(__dirname + '/index.html');
     
 });
