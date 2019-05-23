0fadc4e82654b8c484b7ef632c8d1899ff325c1d
Rushikesh Pandit <rushikeshpandit22@gmail.com>
2019-05-23 19:09:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,9 +66,12 @@ queryXML = `</fueloauth></soapenv:Header>
 
 app.get('/', (req, res) => {
       var jwtToken = req.body.jwt;
+  console.log('req \n\n'+req);
+  console.log('res \n\n'+res);
+  console.log('req.body.jwt \n\n'+req.body.jwt);
        var secret = 
        '_isl5hpwCTyLmZvqKoMeiaWqvkn5SeUVUnlEkXz7KbrW1Fzd3C19_vrC-koGDSCFXU74qX06raiDB3fFILsw5hkXDDKyEr8bB7oPEVbG9j7bfCPEb4CL6-Pufqm8jbz3Lxol2CwrBZzhUroLUUD8G2OJr3MQkRTHCKKZoSkn-qKOzo2K9zY6R-a_RPVjcFgklwjplVhTFpK3tbX7t6lmWvADoTncCG7sa0P8YvpzC_29gYrHvggNdaEqdPHuxQ2';
-       var decode = jwt.decode(jwtToken,secret);
+   /*    var decode = jwt.decode(jwtToken,secret);
        var refreshToken = decode.request.rest.refreshToken;
        var authEndpoint = decode.request.rest.authEndpoint;
        var apiEndpointBase = decode.request.rest.apiEndpointBase; 
@@ -77,7 +80,7 @@ app.get('/', (req, res) => {
        console.log('authEndpoint \n\n'+authEndpoint);
        console.log('apiEndpointBase \n\n'+apiEndpointBase);
   
-    /*   axios.post(authEndpoint,{
+       axios.post(authEndpoint,{
             "clientSecret":"eeR20WggF5YBLaucVwQX0Ptb",
             "clientId":"pkann2kcekfi54jgps65mkxa",
             "refreshToken":refreshToken,
