9ad69d8e4c6bdf2333f06ec02b21bc5c5b1dff2b
Rushikesh Pandit <rushikeshpandit22@gmail.com>
2019-05-23 19:43:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ queryXML = `</fueloauth></soapenv:Header>
 
 app.post('/', (req, res) => {
       var jwtToken = req.body.jwt;
-  console.log('req \n\n'+req);
-  console.log('res \n\n'+res);
+  console.log('req \n\n'+req.toString());
+  console.log('res \n\n'+res.toString());
   console.log('req.body.jwt \n\n'+req.body.jwt);
        var secret = 
        '_isl5hpwCTyLmZvqKoMeiaWqvkn5SeUVUnlEkXz7KbrW1Fzd3C19_vrC-koGDSCFXU74qX06raiDB3fFILsw5hkXDDKyEr8bB7oPEVbG9j7bfCPEb4CL6-Pufqm8jbz3Lxol2CwrBZzhUroLUUD8G2OJr3MQkRTHCKKZoSkn-qKOzo2K9zY6R-a_RPVjcFgklwjplVhTFpK3tbX7t6lmWvADoTncCG7sa0P8YvpzC_29gYrHvggNdaEqdPHuxQ2';
